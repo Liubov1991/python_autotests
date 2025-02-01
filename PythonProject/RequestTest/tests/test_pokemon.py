2cae5c56572697b9f17bc48ecf18b6307e2617f8
QALiubov888 <panfilovaluba91@mail.ru>
2025-02-01 14:53:02 +0300

    Update test_pokemon.py

--- PythonProject/RequestTest/tests/test_pokemon.py
+++ PythonProject/RequestTest/tests/test_pokemon.py
@@ -7,5 +7,9 @@ HEADER = {'Content-Type' : 'application/json','trainer_token': TOKEN}
 TRAINER_ID = '18831'
 
 def test_status_code():
-    response = requests.get(url= f'{URL}/pokemons',params = {'trainer_id': TRAINER_ID}) 
+    response = requests.get(url= f'{URL}/pokemons',params = {'trainer_id' : TRAINER_ID}) 
     assert response.status_code == 200
+def test_trainer_id():
+    response_get = requests.get(url= f'{URL}/trainers',params = {'trainer_id' : TRAINER_ID})
+    assert response_get.json()["data"][0]["trainer_name"] ==  'Сэнсэй'
+    
